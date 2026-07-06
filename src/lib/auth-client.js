4e9ba45ded5968692576e01f94e0cb2833307487
sahadathussain12 <sahadathussain872856@gmail.com>
2026-07-07 00:27:36 +0600

    abc

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -1,7 +1,7 @@
 import { createAuthClient } from "better-auth/react";
 
 export const authClient = createAuthClient({
-  baseURL: "http://localhost:3000",
+  baseURL: "https://pixgen-indol.vercel.app/",
 });
 
 export const { signIn, signUp, signOut, useSession } = authClient;
